8e1f46a811005631943c702e5ab7b350e30a8aa2
sufius <s.aburab@gmail.com>
2022-01-10 21:54:46 +0100

    change comments

--- src/router/index.js
+++ src/router/index.js
@@ -6,67 +6,67 @@ Vue.use(VueRouter)
 const routes = [{
     path: '/',
     name: 'Startseite',
-    component: () => import( /* webpackChunkName: "about" */ '../views/Home.vue')
+    component: () => import( /* webpackChunkName: "home" */ '../views/Home.vue')
   },
   {
     path: '/news',
     name: 'Neuigkeiten',
-    component: () => import( /* webpackChunkName: "about" */ '../views/News.vue')
+    component: () => import( /* webpackChunkName: "news" */ '../views/News.vue')
   },
   {
     name: 'Der Islam',
     path: '/islam/definition',
-    component: () => import( /* webpackChunkName: "about" */ '../views/Islam.vue')
+    component: () => import( /* webpackChunkName: "islam" */ '../views/Islam.vue')
   },
   {
     name: 'Die Propheten',
     path: '/islam/prophets',
-    component: () => import( /* webpackChunkName: "about" */ '../views/Prophets.vue')
+    component: () => import( /* webpackChunkName: "prophets" */ '../views/Prophets.vue')
   },
   {
     name: 'Der Sinn des Lebens',
     path: '/islam/sense-of-life',
-    component: () => import( /* webpackChunkName: "about" */ '../views/SenseOfLive.vue')
+    component: () => import( /* webpackChunkName: "sense_of_live" */ '../views/SenseOfLive.vue')
   },
   {
     name: 'Frauen im Islam',
     path: '/islam/women-and-islam',
-    component: () => import( /* webpackChunkName: "about" */ '../views/WomenAndIslam.vue')
+    component: () => import( /* webpackChunkName: "women_and_islam" */ '../views/WomenAndIslam.vue')
   },
   {
     name: 'Der edle Koran',
     path: '/islam/noble-quran',
-    component: () => import( /* webpackChunkName: "about" */ '../views/NobleQuran.vue')
+    component: () => import( /* webpackChunkName: "noble_quran" */ '../views/NobleQuran.vue')
   },
   {
     path: '/prayer',
     name: 'Gebet',
-    component: () => import( /* webpackChunkName: "about" */ '../views/Prayer.vue')
+    component: () => import( /* webpackChunkName: "prayer" */ '../views/Prayer.vue')
   },
   {
     path: '/donate',
     name: 'Spenden',
-    component: () => import( /* webpackChunkName: "about" */ '../views/Donate.vue')
+    component: () => import( /* webpackChunkName: "donate" */ '../views/Donate.vue')
   },
   {
     path: '/schedule',
     name: 'Aktivitäten',
-    component: () => import( /* webpackChunkName: "about" */ '../views/Schedule.vue')
+    component: () => import( /* webpackChunkName: "schedule" */ '../views/Schedule.vue')
   },
   {
     path: '/location',
     name: 'Kontakt',
-    component: () => import( /* webpackChunkName: "about" */ '../views/Location.vue')
+    component: () => import( /* webpackChunkName: "location" */ '../views/Location.vue')
   },
   {
     path: '/statute',
     name: 'Satzung',
-    component: () => import( /* webpackChunkName: "about" */ '../views/Statute.vue')
+    component: () => import( /* webpackChunkName: "statute" */ '../views/Statute.vue')
   },
   {
     path: '/imprint',
     name: 'Impressum',
-    component: () => import( /* webpackChunkName: "about" */ '../views/Imprint.vue')
+    component: () => import( /* webpackChunkName: "imprint" */ '../views/Imprint.vue')
   }
 ]
 
