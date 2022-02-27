2b956530ba100a3483f188a224b5b5a97018142b
imvlandau <itimvlandau@gmail.com>
2022-02-27 05:27:37 +0100

    cleanup

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,7 @@ module.exports = {
     port: 8080,
     host: '0.0.0.0',
     public: '0.0.0.0:8080'
-    /* public: 'http://10.0.0.10' */
   },
-  /* publicPath: "/", */
   transpileDependencies: [
     'vuetify'
   ]
