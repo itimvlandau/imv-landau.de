6922143f09905d67b95b402959d7dcf100ebe9a8
imvlandau <itimvlandau@gmail.com>
2022-02-27 05:27:22 +0100

    it is working like this too

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,9 @@
 module.exports = {
   devServer: {
-    /* use either poll or vagrant plugin install vagrant-fsnotify */
-    /*
-    watchOptions: {
-        poll: 100
-    },
-    */
     port: 8080,
     host: '0.0.0.0',
-    /* public: '0.0.0.0:8080' */
-    public: 'http://10.0.0.10'
+    public: '0.0.0.0:8080'
+    /* public: 'http://10.0.0.10' */
   },
   /* publicPath: "/", */
   transpileDependencies: [
