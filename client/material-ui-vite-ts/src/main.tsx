6262bd4ec7b1b226596f3056af787d3900a0c1c1
Sufian Abu-Rab <itimvlandau@gmail.com>
2023-08-06 16:53:14 +0200

    add meta data

--- client/material-ui-vite-ts/src/main.tsx
+++ client/material-ui-vite-ts/src/main.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import theme from './theme';
 import App from './App';
 
