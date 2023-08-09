ce91150ae9d8ee31893ce99010be6004874cb09f
Sufian Abu-Rab <itimvlandau@gmail.com>
2023-08-09 20:14:00 +0200

    add path alias for the src directory in order to beatify the import statements

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -12,7 +13,10 @@ export default defineConfig({
       },
     }),
   ],
-  optimizeDeps: {
-      include: ["@emotion/react", "@emotion/styled", "@mui/material", "@mui/icons-material"],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
+    ],
   },
 });
