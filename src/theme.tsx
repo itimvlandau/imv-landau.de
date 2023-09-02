d8fc97361f1d0023ec8ce3466cff297ef3905327
Sufian Abu-Rab <itimvlandau@gmail.com>
2023-09-02 20:22:21 +0200

    update theme add global link component

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,19 +1,53 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import React from 'react';
+import grey from '@mui/material/colors/grey';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
+import { LinkProps } from '@mui/material/Link';
 
-// A custom theme for this app
-const theme = createTheme({
+const LinkBehavior = React.forwardRef<
+  HTMLAnchorElement,
+  Omit<RouterLinkProps, 'to'> & { href: RouterLinkProps['to'] }
+>((props, ref) => {
+  const { href, ...other } = props;
+  // Map href (Material UI) -> to (react-router)
+  return <RouterLink ref={ref} to={href} {...other} />;
+});
+
+let theme = createTheme({
   palette: {
+    mode: 'light',
     primary: {
-      main: '#556cd6',
+      light: "#4dd5f5",
+      main: "#21CBF3",
+      dark: "#178eaa",
     },
     secondary: {
-      main: '#19857b',
+      light: "#4dabf5",
+      main: "#2196F3",
+      dark: "#1769aa",
     },
-    error: {
-      main: red.A400,
+    background: {
+      default: grey[100]
+    }
+  },
+  shape: {
+    borderRadius: 0,
+  },
+  direction: 'ltr',
+  components: {
+    MuiLink: {
+      defaultProps: {
+        component: LinkBehavior,
+      } as LinkProps,
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: LinkBehavior,
+      },
     },
   },
 });
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
