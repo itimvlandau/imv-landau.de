5241fdd25470311de701fdfa2dc22c311dc7c5f8
Sufian Abu-Rab <itimvlandau@gmail.com>
2023-09-02 22:23:18 +0200

    add roboto font as npm package add routes to main file add missing path alias config

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,41 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
-import theme from './theme';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import theme from '@/theme';
+import Layout from "@/Layout";
+import HomePage from "@/pages/Home";
+import EditorPage from "@/pages/Editor";
+import ErrorPage from "@/pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/editor",
+        element: <EditorPage />,
+      }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>,
 );
