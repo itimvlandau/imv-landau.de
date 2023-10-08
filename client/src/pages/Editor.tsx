2cce2d5cb8dde5ba6761f9d0a1668bb4bd7eb188
Sufian Abu-Rab <itimvlandau@gmail.com>
2023-10-08 17:17:09 +0200

    add basic tree example to editor

--- client/src/pages/Editor.tsx
+++ client/src/pages/Editor.tsx
@@ -1,11 +1,68 @@
-import { FunctionComponent, ReactElement } from 'react'
+import { FunctionComponent, ReactElement } from "react";
+import {
+  UncontrolledTreeEnvironment,
+  Tree,
+  StaticTreeDataProvider,
+} from "react-complex-tree";
+import "react-complex-tree/lib/style-modern.css";
 
 interface EditorProps {}
 
+const readTemplate = (template: any, data: any = { items: {} }) => {
+  for (const [key, value] of Object.entries(template)) {
+    data.items[key] = {
+      index: key,
+      canMove: true,
+      isFolder: value !== null,
+      children: value !== null ? Object.keys(value as object) : undefined,
+      data: key,
+      canRename: true
+    };
+
+    if (value !== null) {
+      readTemplate(value, data);
+    }
+  }
+  return data;
+};
+
+const longTree = readTemplate({
+  root: {
+    container: {
+      item0: null,
+      item1: null,
+      item2: null,
+      item3: {
+        inner0: null,
+        inner1: null,
+        inner2: null,
+        inner3: null
+      },
+      item4: null,
+      item5: null
+    }
+  }
+});
+
 const Editor: FunctionComponent<EditorProps> = ({}): ReactElement => {
   return (
-    <div>Editor</div>
-  )
-}
+    <UncontrolledTreeEnvironment
+      dataProvider={
+        new StaticTreeDataProvider(longTree.items, (item, data) => ({
+          ...item,
+          data,
+        }))
+      }
+      getItemTitle={(item) => item.data}
+      viewState={{
+        "tree-1": {
+          expandedItems: ["Fruit"],
+        },
+      }}
+    >
+      <Tree treeId="tree-1" rootItem="root" treeLabel="Tree Example" />
+    </UncontrolledTreeEnvironment>
+  );
+};
 
-export { Editor };
+export default Editor;
