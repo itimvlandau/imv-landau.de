c8a5a70281b27841a6f572aeba6783c9fcb05c6b
Sufian Abu-Rab <itimvlandau@gmail.com>
2024-01-09 23:08:55 +0100

    rename function names

--- api/src/Controller/PmbEditorController.php
+++ api/src/Controller/PmbEditorController.php
@@ -11,14 +11,14 @@ use Symfony\Component\HttpFoundation\Request;
 
 class PmbEditorController extends AbstractController
 {
-    #[Route('/api/pmbEditor', methods: ['GET'], name: 'pmb_editor')]
-    public function show(PmbFilesystemService $pmbFilesystemService): JsonResponse
+    #[Route('/api/pmbEditor', methods: ['GET'], name: 'get_pmb_editor')]
+    public function getPmbEditor(PmbFilesystemService $pmbFilesystemService): JsonResponse
     {
         return new JsonResponse($pmbFilesystemService->scandir());
     }
 
-    #[Route('/api/pmbEditorContent', methods: ['GET'], name: 'pmb_editor_content')]
-    public function getContent(PmbFilesystemService $pmbFilesystemService, Request $request): BinaryFileResponse | JsonResponse
+    #[Route('/api/pmbEditorContent', methods: ['GET'], name: 'get_pmb_editor_content')]
+    public function getPmbEditorContent(PmbFilesystemService $pmbFilesystemService, Request $request): BinaryFileResponse | JsonResponse
     {
         $pathname = $request->query->get('pathname');
         return $pmbFilesystemService->getContent($pathname);
